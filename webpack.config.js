63915128c9e2f455861d03775fb83c02b1ea4eb4
Ryan <ryanjohnparry@gmail.com>
2023-08-28 22:47:06 +0100

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
-const path = require('path');
- 
+const path = require("path");
+
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'www'),
-    filename: 'index.bundle.js',
+    path: path.resolve(__dirname, "www"),
+    filename: "index.bundle.js",
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
+  module: {
+    rules : [
+      // JavaScript/JSX Files
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      // CSS Files
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
 };
